27bd52d2185ddea7cf2f3c17d5ecc08f20e81135
punaisxx <rawin.nx@gmail.com>
2025-04-21 07:31:28 +0700

    add filter

--- 5_rest_api/apis/filters.py
+++ 5_rest_api/apis/filters.py
@@ -1,4 +1,65 @@
 from django_filters import FilterSet, filters
-
+from .models import School, Teacher, Classroom, Student
 
 # code here
+class SchoolFilter(FilterSet):
+    name = filters.CharFilter(lookup_expr='icontains', label="โรงเรียน")
+    
+    class Meta:
+        model = School
+        fields = ['name']
+
+class ClassroomFilter(FilterSet):
+    school = filters.ModelChoiceFilter(queryset=School.objects.all(), label="โรงเรียน")
+    
+    class Meta:
+        model = Classroom
+        fields = ['school']
+
+class TeacherFilter(FilterSet):
+    school = filters.ModelChoiceFilter(
+        queryset=School.objects.all(),
+        label="โรงเรียน"
+    )
+    classroom = filters.ModelChoiceFilter(
+        queryset=Classroom.objects.all(),
+        method='filter_by_classroom',
+        label="ห้องเรียน"
+    )
+    first_name = filters.CharFilter(
+        lookup_expr='icontains',
+        label="ชื่อ"
+    )
+    last_name = filters.CharFilter(
+        lookup_expr='icontains',
+        label="นามสกุล"
+    )
+    gender = filters.ChoiceFilter(
+        choices=Teacher.GENDER_CHOICES,
+        label="เพศ"
+    )
+    
+    class Meta:
+        model = Teacher
+        fields = ['school', 'classroom', 'first_name', 'last_name', 'gender']
+    
+    def filter_by_classroom(self, queryset, name, value):
+        return queryset.filter(classrooms=value)
+
+class StudentFilter(FilterSet):
+    school = filters.ModelChoiceFilter(
+        queryset=School.objects.all(),
+        method='filter_by_school',
+        label="โรงเรียน"
+    )
+    classroom = filters.ModelChoiceFilter(queryset=Classroom.objects.all(), label="ห้องเรียน")
+    first_name = filters.CharFilter(lookup_expr='icontains', label="ชื่อ")
+    last_name = filters.CharFilter(lookup_expr='icontains', label="นามสกุล")
+    gender = filters.ChoiceFilter(choices=Student.GENDER_CHOICES, label="เพศ")
+    
+    class Meta:
+        model = Student
+        fields = ['classroom', 'first_name', 'last_name', 'gender']
+    
+    def filter_by_school(self, queryset, name, value):
+        return queryset.filter(classroom__school=value)
