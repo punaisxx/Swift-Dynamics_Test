398113b5c50c018eeb862ad9c56e9573da91ee05
punaisxx <rawin.nx@gmail.com>
2025-04-21 07:36:29 +0700

    add api routers

--- 5_rest_api/apis/urls.py
+++ 5_rest_api/apis/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
+from .views.v1.school import SchoolViewSet
+from .views.v1.teacher import TeacherViewSet
+from .views.v1.student import StudentViewSet
+from .views.v1.classroom import ClassroomViewSet
 
 router = DefaultRouter()
+router.register(r'schools', SchoolViewSet, basename='school')
+router.register(r'classrooms', ClassroomViewSet, basename='classroom')
+router.register(r'teachers', TeacherViewSet, basename='teacher')
+router.register(r'students', StudentViewSet, basename='student')
 
 api_v1_urls = (router.urls, 'v1')
 
