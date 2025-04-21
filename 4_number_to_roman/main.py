bfc8d1fe8086fda14c7a7903628852f896fbc4e9
punaisxx <rawin.nx@gmail.com>
2025-04-21 07:20:11 +0700

    add 4_number_to_roman

--- 4_number_to_roman/main.py
+++ 4_number_to_roman/main.py
@@ -20,4 +20,16 @@ output = number can not less than 0
 class Solution:
 
     def number_to_roman(self, number: int) -> str:
-        pass
+        num = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        syms = ["M", "CM", "D", "CD","C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
+        if number <= 0:
+            return "number can not less than 0"
+        
+        result = ""
+        for i in range(len(num)):
+            div = number // num[i]
+            number %= num[i]
+            for _ in range(div):
+                result += syms[i]
+        return result
